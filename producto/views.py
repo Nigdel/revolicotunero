cc729a5bd3ec4a3c5504d181b114c21dd3711aeb
Nigdel <nigdel.pena@gmail.com>
2023-03-02 13:44:08 +0000

    bd add

--- producto/views.py
+++ producto/views.py
@@ -4,32 +4,32 @@ from django.contrib import messages
 # Create your views here.
 
 def portada(request):
-    prod = Producto.objects.filter(cantidad = 0)
+    prod = Producto.objects.filter(cantidad__gte = 1)
     # prod = Producto.objects.all()
     if request.method == "POST":
-       codigo = request.POST['codigo'] 
-       nombre = request.POST['nombre'] 
-       precio = request.POST['precio'] 
+       codigo = request.POST['codigo']
+       nombre = request.POST['nombre']
+       precio = request.POST['precio']
        cantidad = request.POST['cantidad']
-       if cantidad and codigo and precio and nombre : 
+       if cantidad and codigo and precio and nombre :
             producto_nuevo = Producto.objects.create(
                 codigo = codigo, nombre = nombre, precio = precio, cantidad= cantidad
             )
        else:
-            messages.success(request,"Datos incompletos para adicionar un producto")  
+            messages.success(request,"Datos incompletos para adicionar un producto")
     return render(request,'portada.html',{'productos':prod})
 
 def edicionProducto(request,codigo):
     prod = Producto.objects.get(codigo=codigo)
     if prod:
         return render(request,'editarProducto.html', {'producto': prod})
-    else:        
+    else:
         messages.success(request,"No se encontro el producto con codigo %d" , codigo)
         return redirect('/')
 def editarproducto(request):
-    codigo = request.POST['codigo'] 
-    nombre = request.POST['nombre'] 
-    precio = request.POST['precio'] 
+    codigo = request.POST['codigo']
+    nombre = request.POST['nombre']
+    precio = request.POST['precio']
     cantidad = request.POST['cantidad']
     prod = Producto.objects.get(codigo=codigo)
     prod.nombre = nombre
@@ -51,8 +51,8 @@ def comprarProducto(request,codigo):
             prod.cantidad= prod.cantidad -1
             prod.save()
             # if prod.cantidad==0:
-            #     prod.delete() 
-            
-    else:        
+            #     prod.delete()
+
+    else:
         messages.success(request,"No se encontro el producto con codigo %d" , codigo)
     return redirect('/')
